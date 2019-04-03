32299512e678b880b0cb9cec96bcb64af11efb14
kseniyaklochko@yahoo.com <a170688>
2019-04-03 17:36:30 +0200

    authentification fix

--- auth/AuthHandler.js
+++ auth/AuthHandler.js
@@ -1,140 +1,65 @@
-const connectToDatabase = require('../db');
-const User = require('../user/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs-then');
+const AUTH0_CLIENT_PUBLIC_KEY = process.env.AUTH0_CLIENT_PUBLIC_KEY;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 
 /*
  * Functions
  */
-module.exports.register = (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    return connectToDatabase()
-        .then(() =>
-            register(JSON.parse(event.body))
-        )
-        .then(session => ({
-            statusCode: 200,
-            body: JSON.stringify(session)
-        }))
-        .catch(err => ({
-            statusCode: err.statusCode || 500,
-            headers: { 'Content-Type': 'text/plain' },
-            body: err.message
-        }));
-};
 
-module.exports.login = (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    return connectToDatabase()
-        .then(() =>
-            login(JSON.parse(event.body))
-        )
-        .then(session => successHandler(session))
-        .catch(err => (errorHandler(err)));
-};
+// Reusable Authorizer function, set on `authorizer` field in serverless.yml
+module.exports.auth = (event, context, callback) => {
+    if (!event.authorizationToken) {
+        return callback('Unauthorized');
+    }
 
+    const tokenParts = event.authorizationToken.split(' ');
+    const tokenValue = tokenParts[1];
 
-module.exports.me = (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    return connectToDatabase()
-        .then(() =>
-            me(event.requestContext.authorizer.principalId) // the decoded.id from the VerifyToken.auth will be passed along as the principalId under the authorizer
-        )
-        .then(session => successHandler(session))
-        .catch(err => (errorHandler(err)));
+    if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+        // no auth token!
+        return callback('Unauthorized');
+    }
+    const options = {
+        audience: AUTH0_CLIENT_ID,
+    };
+
+    try {
+        jwt.verify(tokenValue, AUTH0_CLIENT_PUBLIC_KEY, options, (verifyError, decoded) => {
+            if (verifyError) {
+                console.log('verifyError', verifyError);
+                // 401 Unauthorized
+                console.log(`Token invalid. ${verifyError}`);
+                return callback('Unauthorized');
+            }
+            // is custom authorizer function
+            console.log('valid from customAuthorizer', decoded);
+            return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+        });
+    } catch (err) {
+        console.log('catch error. Invalid token', err);
+        return callback('Unauthorized');
+    }
 };
 
 /*
  * Helpers
  */
-function successHandler(session) {
-    return ({
-        statusCode: 200,
-        body: JSON.stringify(session)
-    });
-}
 
-function errorHandler(err) {
-    return {
-        statusCode: err.statusCode || 500,
-        headers: {'Content-Type': 'text/plain'},
-        body: {stack: err.stack, message: err.message}
-    };
-}
-
-function signToken(id) {
-    console.log("signIn: " + id);
-    return jwt.sign({id: id}, process.env.JWT_SECRET, {
-        expiresIn: 86400 // expires in 24 hours
-    });
-}
-
-function checkIfInputIsValid(eventBody) {
-    if (
-        !(eventBody.password &&
-            eventBody.password.length >= 7)
-    ) {
-        return Promise.reject(new Error('Password error. Password needs to be longer than 8 characters.'));
+// Policy helper function
+const generatePolicy = (principalId, effect, resource) => {
+    const authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        const policyDocument = {};
+        policyDocument.Version = '2019-04-02';
+        policyDocument.Statement = [];
+        const statementOne = {};
+        statementOne.Action = 'execute-api:Invoke';
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
     }
-
-    if (
-        !(eventBody.name &&
-            eventBody.name.length > 5 &&
-            typeof eventBody.name === 'string')
-    ) return Promise.reject(new Error('Username error. Username needs to longer than 5 characters'));
-
-    if (
-        !(eventBody.email &&
-            typeof eventBody.name === 'string')
-    ) return Promise.reject(new Error('Email error. Email must have valid characters.'));
-
-    return Promise.resolve();
-}
-
-function register(eventBody) {
-    return checkIfInputIsValid(eventBody) // validate input
-        .then(() =>
-            User.findOne({email: eventBody.email}) // check if user exists
-        )
-        .then(user =>
-            user
-                ? Promise.reject(new Error('User with that email exists.'))
-                : bcrypt.hash(eventBody.password, 8) // hash the pass
-
-        )
-        .then(hash =>
-                User.create({name: eventBody.name, email: eventBody.email, password: hash})
-            // create the new user
-        )
-        .then(user => ({auth: true, token: signToken(user._id)}));
-    // sign the token and send it back
-}
-
-function login(eventBody) {
-    return User.findOne({email: eventBody.email})
-        .then(user =>
-            !user
-                ? Promise.reject(new Error('User with that email does not exits.'))
-                : comparePassword(eventBody.password, user.password, user._id)
-        )
-        .then(token => ({auth: true, token: token}));
-}
-
-function comparePassword(eventPassword, userPassword, userId) {
-    return bcrypt.compare(eventPassword, userPassword)
-        .then(passwordIsValid =>
-            !passwordIsValid
-                ? Promise.reject(new Error('The credentials do not match.'))
-                : signToken(userId)
-        );
-}
-
-function me(userId) {
-    return User.findById(userId, {password: 0})
-        .then(user =>
-            !user
-                ? Promise.reject('No user found.')
-                : user
-        )
-        .catch(err => Promise.reject(new Error(err)));
-}
+    console.log("authResponse: " + {... authResponse});
+    return authResponse;
+};
